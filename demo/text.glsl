8c383b2e33a559947613eb12fd43e302f7943a0d
Jannik Fehre <Fehre@thpyhs.uni.heidelberg.de>
2020-06-04 18:49:10 +0200

    demo update

--- demo/text.glsl
+++ demo/text.glsl
@@ -3,28 +3,21 @@
 
 #version 330 core
 
-//The 2D vertex positions (in pixels)
-in ivec2 model_position;
+//The 2D vertex positions
+in vec2 model_position;
 //The glyph atlas coordinates.
 in vec2 surface_coords;
 
 //The passed glyph atlas coordinates.
 out vec2 tex_coords;
 
-//The height of the font in pixels.
-uniform int height_int;
-//The height of the font in screen coordinates.
-uniform float height_float;
-//The aspect ratio of the screen.
-uniform float aspect;
 //The offset position of the text in screen coordinates.
 uniform vec2 offset;
 
 void main()
 {
    tex_coords = surface_coords;
-   float scale = height_float / float(height_int);
-   gl_Position = vec4(float(model_position.x) * scale / aspect + offset.x, float(model_position.y) * scale + offset.y, 0, 1);
+   gl_Position = vec4(model_position + offset, 0, 1);
 }
 ###
 
